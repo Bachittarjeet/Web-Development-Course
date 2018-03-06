215a0497d6282f188bef436453125fc9433195e5
Bachittarjeet Singh <bachittarjeet@gmail.com>
2018-03-06 19:29:09 +0530

    Boot-Strap Grid Example

---
+++ Web Development/bootstrap/start.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>First Page</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+</head>
+<body>
+	<button class="btn btn-default">Signup</button>
+	<button class="btn btn-info">info</button>
+	<button class="btn btn-success">success</button>
+	<button class="btn btn-primary">primary</button>
+	<button class="btn btn-warning">warning</button>
+	<button class="btn btn-danger" >danger</button>
+	
+
+	<div class="container">
+		<div class="row ">
+			<div class="col-md-6 col-md-offset-1 text-danger bg-primary">
+				<div class="row">
+					
+					<div class="col-md-6 col-sm-6 col-xs-6 text-danger">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur non tempore perferendis totam et animi quibusdam, eos fuga consectetur asperiores unde saepe expedita, voluptate. Maxime, consequatur earum iusto consequuntur. Odio!
+					<button class="btn btn-lg btn-block btn-danger">HI</button></div>
+					<div class="col-md-6 col-xs-6 col-sm-6" style="color:#fff;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur non tempore perferendis totam et animi quibusdam, eos fuga consectetur asperiores unde saepe expedita, voluptate. Maxime, consequatur earum iusto consequuntur. Odio!<button class="btn btn-lg btn-block btn-danger">HI</button></div>
+				</div>
+				<button class="btn btn-lg btn-block btn-danger">HI</button>
+			</div>
+			<div class="col-md-5 text-success  bg-warning">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint quaerat ratione excepturi quos quam accusamus aut, accusantium molestias quis tenetur blanditiis asperiores dignissimos voluptatibus, consectetur soluta dolore, magnam maiores consequuntur.
+				<button class="btn btn-lg btn-block btn-danger">HI</button>
+			</div>
+		</div>
+	</div>
+	
+
+
+	<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+</body>
+</html>
